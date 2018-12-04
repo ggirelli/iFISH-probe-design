fcd4117216738ee3e901eea89bcb247974c503fd
Gabriele Girelli <gigi.ga90@gmail.com>
2018-12-04 11:01:19 +0100

    Now reading only the needed chromosomes from the database.

--- fish_prode/query.py
+++ fish_prode/query.py
@@ -25,12 +25,13 @@ from tqdm import tqdm
 class OligoDatabase(object):
     """FISH-ProDe Oligonucleotide Database class."""
 
-    def __init__(self, dbDirPath, verbose = False, hasNetwork = True,
+    def __init__(self, dbDirPath, hasNetwork = True,
         UCSC_DAS_URI = fp.web.UCSC_DAS_URI):
         super(OligoDatabase, self).__init__()
         self.dirPath = dbDirPath
         self.hasNetwork = hasNetwork
         self.UCSC_DAS_URI = UCSC_DAS_URI
+        self.chromData = {}
 
         assert_msg = f'expected folder, file found: "{self.dirPath}"'
         assert not os.path.isfile(self.dirPath), assert_msg
@@ -52,8 +53,6 @@ class OligoDatabase(object):
             refGenomeChecked = fp.web.check_reference_genome(
                 refGenome, self.UCSC_DAS_URI)
             assert refGenomeChecked, f'genome "{refGenome}" not found @UCSC'
-
-        self._read_chromosomes(verbose)
 
     def check_overlaps(self):
         hasOverlaps = False
@@ -87,72 +86,79 @@ class OligoDatabase(object):
         '''Reads sequence status from Database .config'''
         return self.config.getboolean('OLIGOS', 'sequence')
 
-    def _read_chromosomes(self, verbose):
+    def has_chromosome(self, chrom):
+        return chrom in os.listdir(self.dirPath)
 
-        chromList = [d for d in os.listdir(self.dirPath) if not d.startswith('.')]
+    def read_chromosome(self, chrom):
+        assert self.has_chromosome(chrom)
+        
+        chromPath = os.path.join(self.dirPath, chrom)
+        chromData = pd.read_csv(chromPath, '\t', header = None)
+        chromData.columns = fp.bioext.UCSCbed.FIELD_NAMES[
+            1:(chromData.shape[1]+1)]
+
+        assert_msg = f'found empty chromosome file: "{chromPath}"'
+        assert 0 != chromData.shape[0], assert_msg
+
+        assert_msg = f'missing columns in "{chromPath}"'
+        assert 2 <= chromData.shape[1], assert_msg
+
+        assert_msg = f'found unsorted file: "{chromPath}"'
+        assert all(chromData.iloc[:, 0].diff()[1:] > 0), assert_msg
+
+        assert_msg = f'found unsorted file: "{chromPath}"'
+        assert all(chromData.iloc[:, 1].diff()[1:] >= 0), assert_msg
+
+        oligoLengthRange = self.get_oligo_length_range()
+        oligoLengthList = chromData.iloc[:, 1] - chromData.iloc[:, 0]
+        assert_msg = f'oligo too small for ".config" in "{chromPath}"'
+        assert all(oligoLengthList >= oligoLengthRange[0]), assert_msg
+        assert_msg = f'oligo too big for ".config" in "{chromPath}"'
+        assert all(oligoLengthList <= oligoLengthRange[1]), assert_msg
+
+        if self.has_overlaps():
+            assert_msg = f'overlaps status mismatch in "{chromPath}"'
+            assert self.check_overlaps(), assert_msg
+
+        if self.has_sequences():
+            assert_msg = f'missing sequence columns in "{chromPath}"'
+            assert chromData.shape[1] >= 3, assert_msg
+
+        if self.hasNetwork and self.has_sequences():
+            for i in range(chromData.shape[0]):
+                chromStart, chromEnd, sequence = tuple(
+                    chromData.iloc[i, :3].tolist())
+
+                assert_msg = 'sequence length does not match oligo'
+                assert_msg += f' (#{i}) length'
+                assert len(sequence) == chromEnd-chromStart, assert_msg
+
+                assert_status, msg = fp.web.check_sequence(
+                    (chrom, chromStart, chromEnd), sequence,
+                    self.get_reference_genome(),
+                    UCSC_DAS_URI = self.UCSC_DAS_URI)
+                assert_msg = f'sequence of oligo #{i} does not match'
+                assert_msg += ' UCSC' + msg
+                assert assert_status, assert_msg
+        
+        if self.hasNetwork:
+            chromSizeCheck = fp.web.check_chromosome_size(
+                chrom, chromData.iloc[-1, 1],
+                self.get_reference_genome(),
+                UCSC_DAS_URI = self.UCSC_DAS_URI)
+            assert_msg = f'chromosome size not respected: "{chromPath}"'
+            assert chromSizeCheck, assert_msg
+
+        self.chromData[chrom] = chromData
+
+    def read_all_chromosomes(self, verbose):
+        chromList = [d for d in os.listdir(self.dirPath)
+            if not d.startswith('.')]
         assert 1 < len(chromList), "no chromosome files found in {self.dirPath}"
         chromList = tqdm(chromList) if verbose else chromList
 
-        self.chromData = {}
         for chrom in chromList:
-            chromPath = os.path.join(self.dirPath, chrom)
-            chromData = pd.read_csv(chromPath, '\t', header = None)
-            chromData.columns = fp.bioext.UCSCbed.FIELD_NAMES[
-                1:(chromData.shape[1]+1)]
-
-            assert_msg = f'found empty chromosome file: "{chromPath}"'
-            assert 0 != chromData.shape[0], assert_msg
-
-            assert_msg = f'missing columns in "{chromPath}"'
-            assert 2 <= chromData.shape[1], assert_msg
-
-            assert_msg = f'found unsorted file: "{chromPath}"'
-            assert all(chromData.iloc[:, 0].diff()[1:] > 0), assert_msg
-
-            assert_msg = f'found unsorted file: "{chromPath}"'
-            assert all(chromData.iloc[:, 1].diff()[1:] >= 0), assert_msg
-
-            oligoLengthRange = self.get_oligo_length_range()
-            oligoLengthList = chromData.iloc[:, 1] - chromData.iloc[:, 0]
-            assert_msg = f'oligo too small for ".config" in "{chromPath}"'
-            assert all(oligoLengthList >= oligoLengthRange[0]), assert_msg
-            assert_msg = f'oligo too big for ".config" in "{chromPath}"'
-            assert all(oligoLengthList <= oligoLengthRange[1]), assert_msg
-
-            if self.has_overlaps():
-                assert_msg = f'overlaps status mismatch in "{chromPath}"'
-                assert self.check_overlaps(), assert_msg
-
-            if self.has_sequences():
-                assert_msg = f'missing sequence columns in "{chromPath}"'
-                assert chromData.shape[1] >= 3, assert_msg
-
-            if self.hasNetwork and self.has_sequences():
-                for i in range(chromData.shape[0]):
-                    chromStart, chromEnd, sequence = tuple(
-                        chromData.iloc[i, :3].tolist())
-
-                    assert_msg = 'sequence length does not match oligo'
-                    assert_msg += f' (#{i}) length'
-                    assert len(sequence) == chromEnd-chromStart, assert_msg
-
-                    assert_status, msg = fp.web.check_sequence(
-                        (chrom, chromStart, chromEnd), sequence,
-                        self.get_reference_genome(),
-                        UCSC_DAS_URI = self.UCSC_DAS_URI)
-                    assert_msg = f'sequence of oligo #{i} does not match'
-                    assert_msg += ' UCSC' + msg
-                    assert assert_status, assert_msg
-            
-            if self.hasNetwork:
-                chromSizeCheck = fp.web.check_chromosome_size(
-                    chrom, chromData.iloc[-1, 1],
-                    self.get_reference_genome(),
-                    UCSC_DAS_URI = self.UCSC_DAS_URI)
-                assert_msg = f'chromosome size not respected: "{chromPath}"'
-                assert chromSizeCheck, assert_msg
-
-            self.chromData[chrom] = chromData
+            self.read_chromosome(chrom)
 
 class OligoProbe(object):
     """Class for probe management."""
