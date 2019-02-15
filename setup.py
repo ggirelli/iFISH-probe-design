65e56c9f32ff55d8cfef1030d40218471d3b8608
Gabriele Girelli <gigi.ga90@gmail.com>
2019-02-15 08:18:28 +0100

    Added badges.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', "r") as f:
 	long_description = f.read()
 
 setup(name='ifpd',
-	version='2.0.2',
+	version='2.0.2.post1',
 	description='''An iFISH probe design pipeline, with web interface included.''',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
